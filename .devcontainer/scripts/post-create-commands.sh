5c50d3b4d34e1206f7597d8f754d3002834ae39d
Nhlanhla Malaza <nhlanhla@wallety.cash>
2026-01-03 17:12:39 +0200

    devcontainer functioning

--- .devcontainer/scripts/post-create-commands.sh
+++ .devcontainer/scripts/post-create-commands.sh
@@ -3,15 +3,16 @@
 : "${GIT_USER_NAME:?Need to set GIT_USER_NAME}"
 
 # Git config
-chmod 600 /root/.ssh/id_rsa
-eval $(ssh-agent -s)
-ssh-add /root/.ssh/id_rsa
+chmod 600 $HOME/.ssh/id_ed25519
+eval "$(ssh-agent -s)"
+ssh-add $HOME/.ssh/id_ed25519
 
-echo "chmod 600 /root/.ssh/id_rsa" >>~/.bashrc
+# Add to .bashrc
+echo "chmod 600 \$HOME/.ssh/id_ed25519" >>~/.bashrc
 echo "eval \$(ssh-agent -s)" >>~/.bashrc
-echo "ssh-add /root/.ssh/id_rsa" >>~/.bashrc
+echo "ssh-add \$HOME/.ssh/id_ed25519" >>~/.bashrc
 
-git config --global --add safe.directory /workspaces
+git config --global --add safe.directory /workspaces/njmtech-yt-transcribe
 
 git config --global user.email "$GIT_USER_EMAIL" &&
   git config --global user.name "$GIT_USER_NAME"
