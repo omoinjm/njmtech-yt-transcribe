58616dc28ec2a6af8249410698b7202f3ceb74bb
omoinjm <njmcloud@gmail.com>
2026-01-11 16:25:52 +0200

    i love it

--- scripts/transcribe.sh
+++ scripts/transcribe.sh
@@ -4,8 +4,8 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$(dirname "$SCRIPT_DIR")"
 
-LINK="https://www.youtube.com/watch?v=rdWZo5PD9Ek"
-# LINK="https://www.instagram.com/reel/DTQNaqBE0zw/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+# LINK="https://www.youtube.com/watch?v=rdWZo5PD9Ek"
+LINK="https://www.instagram.com/reel/DTQNaqBE0zw/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
 OUTPUT_DIR="/tmp/njmtech-yt-transcribe/"
 
 ./yt-transcribe -url $LINK -output $OUTPUT_DIR
